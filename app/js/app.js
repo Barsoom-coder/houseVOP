452e23e428353b50620fc08eef1681cf74197974
roman_kononov <romankononovdev@mail.ru>
2020-10-17 23:16:16 +0700

    modified dist

--- app/js/app.js
+++ app/js/app.js
@@ -132,3 +132,25 @@ function start_lazy_map() {
 $(window).on('load', function() {
 	$('.preloader').delay(1000).fadeOut('slow');
 });
+
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("questions__form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
